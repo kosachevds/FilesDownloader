7efd6f1e0eb9200d0bcad36092fd8a56a6798ea9
Daniil Kosachev <kos-dan@mail.ru>
2019-09-03 22:15:03 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FilesDownloader
+{
+    class Program
+    {
+
+        class DownloadingInfo {
+            public string Filename { get; set; }
+            public string Url { get; set; }
+
+            static DownloadingInfo Parse(string strInfo) {
+                var lastSpace = strInfo.LastIndexOf(' ');
+                var url = strInfo.Substring(lastSpace + 1);
+                var filename = strInfo.Substring(0, lastSpace);
+                return new DownloadingInfo {
+                    Filename = filename,
+                    Url = url
+                };
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static IEnumerable<DownloadingInfo> ReadInfos(string filename, char separator) {
+
+        }
+
+    }
+
+
+
+}
